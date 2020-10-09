fea9bc6b46f7e95726ac5f9f97746c9d79af9351
saksham2007 <70582707+saksham2007@users.noreply.github.com>
2020-10-09 08:22:14 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -3,27 +3,26 @@ class Paper
     constructor(x,y,r)
     {
         var options={
-            isStatic:false,
+            //isStatic:false,
             restitution:0.3,
-            friction:0.9,
+            //friction:0.9,
             density:1.2
         }
-        this.x=x;
-        this.y=y;
+
         this.r=r;
-        this.body=Bodies.circle(this.x,this.y,this.r/2,options)
+        this.body=Bodies.circle(x,y,this.r,options)
         World.add(world,this.body);
     }
     display()
     {
         var paperpos=this.body.position;
 
-        push()
-        translate(paperpos.x,paperpos.y);
-        rectMode(CENTER)
+       // push()
+       // translate(paperpos.x,paperpos.y);
+       ellipseMode(RADIUS);
         strokeWeight(3);
         fill(255,0,255)
         ellipse(0,0,this.r,this.r)
-        pop()
+       // pop()
     }
 }
